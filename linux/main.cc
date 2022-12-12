a8303e661b71654eaa74d31e61b9372cffec9696
unknown <mengqingbing@qianxin.com>
2022-12-12 16:33:32 +0800

    添加active检查

--- linux/main.cc
+++ linux/main.cc
@@ -3,15 +3,25 @@ using namespace Mango;
 
 void ss(void* buffer, size_t buffer_size, CLIENT_CONTEXT ctx){
     char s[] = "HTTP/1.0 200 OK\n"
-                "Server: SimpleHTTP/0.6 Python/3.10.6\n"
+                "Server: RedDragon/1.0.0 (Ubuntu)\n"
                 "Date: Fri, 09 Dec 2022 08:35:52 GMT\n"
                 "Content-type: text/html\n"
                 "Content-Length: 16\n"
                 "Last-Modified: Fri, 09 Dec 2022 08:31:32 GMT\n\n"
-                "<!DOCTYPE html>";
+                "<!DOCTYPE html>\n"
+                "<html>\n"
+                "\t<head>\n"
+                "\t\t<meta charset=\"utf-8\">\n"
+                "\t\t<title>llllll</title>\n"
+                "\t</head>\n"
+                "\t<body>\n"
+                "\t\t<center><h1>400 Bad Request</h1></center>\n"
+                "\t\t<p>111111111</p>\n"
+                "\t</body>\n"
+                "</html>\n";
                 
     int l = send(ctx.fd, s, sizeof(s), 0);
-    close(ctx.fd);
+    NetworkEpoll::CloseSocketFD(ctx.fd);
 }
 
 int main(int argc, char* argv[]) {
